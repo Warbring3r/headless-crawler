f3da20fb47d3ef8bc3eac19148430015db9d0eac
Warbring3r <david.matthew.west@gmail.com>
2017-11-06 11:35:11 -0800

    Update example.js

--- src/example.js
+++ src/example.js
@@ -4,14 +4,16 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({args: ['--no-sandbox']});
   console.log(await browser.version());
   const page = await browser.newPage();
+  // Gmail has an http redirect
   await page.goto('http://gmail.com')
   console.log("url", await page.url())
+  // Forbes has a JS redirect
   await page.goto('https://www.forbes.com/sites/samabuelsamid/2017/04/25/waymo-launches-early-rider-program-expands-self-driving-fleet-with-fiat-chrysler/&refURL=&referrer=')
-      console.log("url", await page.url())
-    console.log("going to sleep");
-    await sleep(20000);
-    console.log('waking up');
-    console.log("url", await page.url())
+  console.log("url", await page.url())
+  console.log("going to sleep");
+  await sleep(20000);
+  console.log('waking up');
+  console.log("url", await page.url())
   browser.close();
 })();
 
